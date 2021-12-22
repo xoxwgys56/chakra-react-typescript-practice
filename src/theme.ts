5a892f7aacc2702fc16c7e6ebc1681c796f66926
xoxwgys56 <lololtoday@gmail.com>
2021-12-23 00:55:33 +0900

    docs: add todo

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,8 @@
 import { extendTheme } from "@chakra-ui/react";
 import "@fontsource/roboto";
 
+// TODO change color scheme for dark mode
+
 const theme = extendTheme({
 	fonts: {
 		heading: "Roboto",
