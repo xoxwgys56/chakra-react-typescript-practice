e10fa8a5280325583a6e416c4d316af31527fb98
xoxwgys56 <lololtoday@gmail.com>
2021-12-22 23:24:16 +0900

    chore: add padding
    top and bottom

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,15 @@ import { Heading } from "@chakra-ui/react";
 
 function Header() {
 	return (
-		<Heading as="h1" size="4xl" color="gray.300" fontWeight={100} lineHeight="1.4rem">
+		<Heading
+			as="h1"
+			size="4xl"
+			color="gray.300"
+			fontWeight={100}
+			lineHeight="1.4rem"
+			paddingTop="50px"
+			paddingBottom="10px"
+		>
 			todos
 		</Heading>
 	);
