1fc57efc0e22d33ed30273f95a2e5c5fffcc4a65
kim daewon <lololtoday@gmail.com>
2021-12-22 17:45:02 +0900

    chore: update progress
    footer, todoitem wip

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,20 @@
 import React from "react";
-import { Box, Checkbox, Container, Input, HStack, VStack, Divider } from "@chakra-ui/react";
+import {
+	Box,
+	Checkbox,
+	Container,
+	Input,
+	HStack,
+	VStack,
+	Divider,
+	Grid,
+	Button,
+	GridItem,
+	SimpleGrid,
+	ButtonGroup,
+	Text,
+	Center,
+} from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 
 interface ItemLayoutProps {
@@ -14,6 +29,8 @@ type FontConfig = {
 
 interface ItemProps {
 	fontConfig: FontConfig;
+	itemValue?: string;
+	isCompleted?: boolean;
 }
 
 /**
@@ -37,8 +54,10 @@ function ItemLayout({ children }: ItemLayoutProps) {
 }
 
 function TodoItem({ fontConfig }: ItemProps) {
-	// TODO hover "x" 표시 우측에 띄우기
+	// TODO hover "x" 표시 우측에 띄우기 -> use close button
 	// TODO checkbox circle 만들기
+	// TODO change font cancel line when item completed
+
 	return (
 		<ItemLayout>
 			<Checkbox size="lg" colorScheme="gray" icon={<ChevronDownIcon />}></Checkbox>
@@ -83,6 +102,65 @@ function TodoInput({ fontConfig }: ItemProps) {
 	);
 }
 
+function TodoFooter({ leftCount = 0 }: { leftCount: number }) {
+	// TODO change send value to use `createTheme` for `themeProvider`
+	const fontConfig = {
+		size: "1rem",
+		color: "gray.900",
+		weight: 200,
+	} as FontConfig;
+
+	interface ButtonState {
+		name: string;
+		state: boolean;
+	}
+
+	// TODO store this value as initial value
+	const buttonState = new Array<ButtonState>(
+		{ name: "All", state: true },
+		{ name: "Active", state: false },
+		{ name: "Completed", state: false }
+	);
+
+	return (
+		<Grid
+			w="100%"
+			templateColumns="repeat(3, 1fr)"
+			paddingTop="12.5px"
+			fontSize={fontConfig.size}
+			color={fontConfig.color}
+			fontWeight={fontConfig.weight}
+		>
+			<Center justifySelf="start">{leftCount} items left</Center>
+			<ButtonGroup>
+				{/*TODO apply button style by current state*/}
+				{buttonState.map((button) => (
+					<Button
+						fontSize={fontConfig.size}
+						fontWeight={fontConfig.weight}
+						variant={button.state ? "outline" : "ghost"}
+						_focus={{ border: 0 }}
+					>
+						{button.name}
+					</Button>
+				))}
+			</ButtonGroup>
+			{/* TODO add button state which current todolist has completed item */}
+			<Center justifySelf="end">
+				<Button
+					variant="link"
+					_focus={{ border: 0 }}
+					fontSize={fontConfig.size}
+					fontWeight={fontConfig.weight}
+					color={fontConfig.color}
+				>
+					Clear completed
+				</Button>
+			</Center>
+		</Grid>
+	);
+}
+
 function TodoList() {
 	// TODO footer 추가
 	const fontConfig = {
@@ -91,12 +169,15 @@ function TodoList() {
 		weight: 300,
 	} as FontConfig;
 
+	const leftCount = 0;
+
 	return (
 		<Container bg="white" w="80%" paddingTop="25px" paddingBottom="25px" boxShadow="lg">
 			<VStack>
 				<TodoInput fontConfig={fontConfig} />
 				{/*<Divider w="100%" paddingTop="15px" />*/}
 				<TodoItem fontConfig={fontConfig} />
+				<TodoFooter leftCount={leftCount} />
 			</VStack>
 		</Container>
 	);
