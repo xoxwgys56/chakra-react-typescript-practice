ffc75119059c2a93af99681c88b117eb29861e1f
kim daewon <lololtoday@gmail.com>
2021-12-22 17:45:37 +0900

    chore: change font size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Heading } from "@chakra-ui/react";
 
 function Header() {
 	return (
-		<Heading as="h1" size="3xl" color="gray.300" fontWeight={100}>
+		<Heading as="h1" size="4xl" color="gray.300" fontWeight={100} lineHeight="1.4rem">
 			todos
 		</Heading>
 	);
