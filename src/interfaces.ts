a0dadd7474ca25f495b140b0ced6f4e7a1159878
xoxwgys56 <lololtoday@gmail.com>
2021-12-24 10:26:59 +0900

    chore: add props & state
    - ItemInputProps
    - TodoState

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,11 +1,23 @@
+import { BaseSyntheticEvent } from "react";
+
 export interface FontConfig {
 	size: string;
 	color: string;
 	weight: number;
 }
 
+export interface ItemInputProps {
+	fontConfig: FontConfig;
+	createNewItem: (e: BaseSyntheticEvent) => void;
+}
+
 export interface ItemProps {
 	fontConfig: FontConfig;
 	itemValue?: string;
 	isCompleted?: boolean;
+}
+
+export interface TodoState {
+	todoList: string[];
+	currentInput: string;
 }
