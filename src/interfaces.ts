f635216b9ceee6ed0f18f7936e3b5be74144d585
xoxwgys56 <lololtoday@gmail.com>
2022-01-03 14:11:34 +0900

    chore: update type and interfaces
    add `TodoAction` and `TodoState` for typing.
    add some prop parameter's prop (which function call dispath)

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,4 +1,4 @@
-import { BaseSyntheticEvent } from "react";
+import { BaseSyntheticEvent, MouseEventHandler } from "react";
 
 export interface FontConfig {
 	size: string;
@@ -9,15 +9,28 @@ export interface FontConfig {
 export interface ItemInputProps {
 	fontConfig: FontConfig;
 	createNewItem: (e: BaseSyntheticEvent) => void;
+	updateInputValue: (inputValue: string) => void;
+	// dispatch: (state: TodoState, action: TodoAction) => React.Dispatch<TodoAction>;
 }
 
 export interface ItemProps {
 	fontConfig: FontConfig;
-	itemValue?: string;
+	itemText?: string;
 	isCompleted?: boolean;
+	removeItem: (itemText: string) => void;
 }
 
-export interface TodoState {
-	todoList: string[];
+export type ItemInfo = {
+	text: string;
+	isCompleted: boolean;
+};
+
+export type TodoState = {
+	todoList: ItemInfo[];
 	currentInput: string;
-}
+};
+
+export type TodoAction =
+	| { type: "CREATE_TODO"; itemText: string }
+	| { type: "REMOVE_TODO"; itemText: string }
+	| { type: "UPDATE_INPUT_VALUE"; inputValue: string };
