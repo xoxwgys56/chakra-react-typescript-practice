2d9cce6c09435c57cee259a7b86af6a58e18efc8
xoxwgys56 <lololtoday@gmail.com>
2022-01-16 18:19:50 +0900

    chore: comment color mode
    color mode not implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { ChakraProvider, Box, VStack, Grid } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
 // import { Logo } from "./Logo";
 import Header from "./components/Header";
 import ItemList from "./components/ItemList";
@@ -12,7 +11,7 @@ export const App = () => (
 		<Meta />
 		<Box textAlign="center" fontSize="xl" bg="gray.50">
 			<Grid minH="100vh" p={3}>
-				<ColorModeSwitcher justifySelf="flex-end" />
+				{/*<ColorModeSwitcher justifySelf="flex-end" />*/}
 				<VStack spacing={8} justifySelf="center" position="absolute">
 					<Header />
 					<ItemList />
